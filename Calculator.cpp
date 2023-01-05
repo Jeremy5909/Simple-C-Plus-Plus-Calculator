183011a4333d7993ea2141e01a90f3d866414409
anthuny <118776713+aeisenhut1@users.noreply.github.com>
2023-01-04 22:58:41 -0700

    Update Calculator.cpp
    Grammatical improvement

--- Calculator.cpp
+++ Calculator.cpp
@@ -17,7 +17,7 @@ double ans;
 cout << "Simple C++ Calculator 1.0.1\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
 cin >> calc;
 if (calc == hlp) {
-	cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
+	cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly, see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
 }
 if (calc == mult) {
 	cout << "Multiplication calculator\n\nFirst number: ";
