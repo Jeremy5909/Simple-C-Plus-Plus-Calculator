d440aadc1e1076446121b8bc66dc4d251b1c4afd
anthuny <118776713+aeisenhut1@users.noreply.github.com>
2023-01-05 01:47:33 -0700

    Touch-ups

--- Calculator.cpp
+++ Calculator.cpp
@@ -14,21 +14,19 @@ double num1;
 double num2;
 double ans;
 
-cout << "Simple C++ Calculator 1.0.1\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
+cout << "Simple C++ Calculator 1.0.2\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
 cin >> calc;
 if (calc == hlp) {
 	cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly, see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
-}
-if (calc == mult) {
-	cout << "Multiplication calculator\n\nFirst number: ";
+} else if (calc == mult) {
+	cout << "\nMultiplication calculator\n\nFirst number: ";
 	cin >> num1;
 	cout << "\nSecond number: ";
 	cin >> num2;
 	ans = num1 * num2;
 	cout << "\nAnswer: " << ans;
 	cout << "\n";
-}
-if (calc == div) {
+} else if (calc == div) {
 	cout << "\nDivision calculator\n\nFirst number: ";
 	cin >> num1;
 	cout << "\nSecond number: ";
@@ -36,8 +34,7 @@ if (calc == div) {
 	ans = num1 / num2;
 	cout << "\nAnswer: " << ans;
         cout << "\n";
-}
-if (calc == add) {
+} else if (calc == add) {
 	cout << "\nAddition calculator\n\nFirst number: ";
 	cin >> num1;
 	cout << "\nSecond number: ";
@@ -45,8 +42,7 @@ if (calc == add) {
 	ans = num1 + num2;
 	cout << "\nAnswer: " << ans;
         cout << "\n";
-}
-if (calc == sub) {
+} else if (calc == sub) {
 	cout << "\nSubtraction calculator\n\nFirst number: ";
 	cin >> num1;
 	cout << "\nSecond number: ";
