115c105770bd2ee4fb58603a2317bd796a79fd17
anthuny <118776713+aeisenhut1@users.noreply.github.com>
2023-01-05 02:34:10 -0700

    Renamed version in description

--- Calculator.cpp
+++ Calculator.cpp
@@ -15,7 +15,7 @@ int main() {
         double num2;
         double ans;
 
-        cout << "Simple C++ Calculator 1.0.2\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
+        cout << "Simple C++ Calculator 1.0.3\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
         cin >> calc;
         if (calc == hlp) {
                 cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly, see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
