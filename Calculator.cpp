8414740c5bdf0385543f8664f28fe75464e7388e
anthuny <118776713+aeisenhut1@users.noreply.github.com>
2023-01-04 17:55:51 -0700

    Added help screen

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,46 +9,50 @@ string mult = "Multiplication";
 string div = "Division";
 string add = "Addition";
 string sub = "Subtraction";
+string hlp = "Help";
 double num1;
 double num2;
 double ans;
 
-cout << "Simple C++ calculator 1.0.0\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\n\nSelect your calculation: ";
+cout << "Simple C++ Calculator 1.0.1\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
 cin >> calc;
+if (calc == hlp) {
+	cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
+}
 if (calc == mult) {
 	cout << "Multiplication calculator\n\nFirst number: ";
 	cin >> num1;
-	cout << "Second number: ";
+	cout << "\nSecond number: ";
 	cin >> num2;
 	ans = num1 * num2;
-	cout << "Answer: " << ans;
+	cout << "\nAnswer: " << ans;
 	cout << "\n";
 }
 if (calc == div) {
-	cout << "Division calculator\n\nFirst number: ";
+	cout << "\nDivision calculator\n\nFirst number: ";
 	cin >> num1;
-	cout << "Second number: ";
+	cout << "\nSecond number: ";
 	cin >> num2;
 	ans = num1 / num2;
-	cout << "Answer: " << ans;
+	cout << "\nAnswer: " << ans;
         cout << "\n";
 }
 if (calc == add) {
-	cout << "Addition calculator\n\nFirst number: ";
+	cout << "\nAddition calculator\n\nFirst number: ";
 	cin >> num1;
-	cout << "Second number: ";
+	cout << "\nSecond number: ";
 	cin >> num2;
 	ans = num1 + num2;
-	cout << "Answer: " << ans;
+	cout << "\nAnswer: " << ans;
         cout << "\n";
 }
 if (calc == sub) {
-	cout << "Subtraction calculator\n\nFirst number: ";
+	cout << "\nSubtraction calculator\n\nFirst number: ";
 	cin >> num1;
-	cout << "Second number: ";
+	cout << "\nSecond number: ";
 	cin >> num2;
 	ans = num1 - num2;
-	cout << "Answer: " << ans;
+	cout << "\nAnswer: " << ans;
         cout << "\n";
 }
 return 0;
