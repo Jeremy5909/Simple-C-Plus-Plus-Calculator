8efa1cd8feeda3acc1ef8f3232b7f63250d8ef4e
anthuny <118776713+aeisenhut1@users.noreply.github.com>
2023-01-08 21:43:25 -0700

    made easier to use

--- main.cpp
+++ main.cpp
@@ -5,17 +5,18 @@
 using namespace std;
 
 int main() {
-        string calc;
-        string mult = "Multiplication";
-        string divi = "Division";
-        string add = "Addition";
-        string sub = "Subtraction";
-        string hlp = "Help";
+        int calc;
+        int hlp = 1;
+        int mult = 2;
+        int divi = 3;
+        int add = 4;
+        int sub = 5;
+
         double num1;
         double num2;
         double ans;
 
-        cout << "Simple C++ Calculator 1.0.3\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: ";
+        cout << "Simple C++ Calculator 1.0.3\n\nTypes of calculaton:\n\n[1]\tHelp\n[2]\tMultiplication\n[3]\tDivision\n[4]\tAddition\n[5]\tSubtraction\n\nSelect your calculation: ";
         cin >> calc;
         if (calc == hlp) {
                 cout << "\nInstructions:\nFirst, type in one of the types of calcultation from the list and press enter. Next, type in the first number of the calculation after \"First Number: \" and press enter. After that, type the second number of the calculation after \"Second number: \" and press enter. Lastly, see the answer you got and restart to run again.\n\nExample:\n\nTypes of calculaton:\n\nMultiplication\nDivision\nAddition\nSubtraction\nHelp\n\nSelect your calculation: Division\n\nDivision calculator\n\nFirst number: 6\n\nSecond number: 5\n\nAnswer: 1.2\n";
@@ -52,7 +53,7 @@ int main() {
                 cout << "\nAnswer: " << ans;
                 cout << "\n";
         } else {
-                cout << "\n" << calc << "is not an option.\n";
+                cout << "\n" << calc << " is not an option.\n";
         }
         return 0;
 }
